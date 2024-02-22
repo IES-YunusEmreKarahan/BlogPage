5fa531cac9a9e2e6796818769fe7e0ba1a172fd3
IES-YunusEmreKarahan <147061126+IES-YunusEmreKarahan@users.noreply.github.com>
2024-02-22 16:55:42 +0300

    Blog Page

---
+++ Blog Site/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+	setTimeout( function() {
+		var wrapper = document.getElementById("wrapper");
+		wrapper.classList.add("loaded");
+	}, 500);
+});
